263d778cdebd68d1c3631c570cfdd72cbcdae8e4
ronald-developer <84114897+ronald-developer@users.noreply.github.com>
2025-09-09 18:46:19 +0800

    Initial setup

---
+++ InventorySales.EntityFramework/InventorySalesDbContext.cs
@@ -0,0 +1,42 @@
+﻿
+using InventorySales.EntityFramework.Configurations;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+
+// using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace InventorySales.EntityFramework
+{
+    public class InventorySalesDbContext : IdentityDbContext<AppUser>
+    {
+        public InventorySalesDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        // public DbSet<Expense> Expenses { get; set; }
+        // public DbSet<ExpenseCategory> ExpenseCategories { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            ApplyBaseConfiguration(modelBuilder);
+
+            modelBuilder.ApplyConfiguration(new RoleConfiguration());
+        }
+
+        private static void ApplyBaseConfiguration(ModelBuilder modelBuilder)
+        {
+            foreach (var entityType in modelBuilder.Model.GetEntityTypes())
+            {
+                if (typeof(BaseEntity).IsAssignableFrom(entityType.ClrType))
+                {
+                    modelBuilder.Entity(entityType.ClrType)
+                        .Property("Id")
+                        .ValueGeneratedOnAdd();
+                }
+            }
+        }
+    }
+
+}
+
